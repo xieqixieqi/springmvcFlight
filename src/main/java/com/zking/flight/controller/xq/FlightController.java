1c87125658878738f30a0258846d438a9207d3fe
xieqixieqi <2628585756@qq.com>
2018-11-22 21:14:23 +0800

    3

--- src/main/java/com/zking/flight/controller/xq/FlightController.java
+++ src/main/java/com/zking/flight/controller/xq/FlightController.java
@@ -1,9 +1,6 @@
 package com.zking.flight.controller.xq;
 
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
